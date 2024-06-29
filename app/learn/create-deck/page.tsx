a6ad988f964a8b70cd180be46b9b3257e09f591c
kiel <kielmitchell8@gmail.com>
2024-06-29 19:07:23 +0100

    strips down create deck to a small form, will conditionally place in learn page rather than have a separate route

--- app/learn/create-deck/page.tsx
+++ app/learn/create-deck/page.tsx
@@ -4,21 +4,12 @@ import { AddDeckDashboard } from '@/components/AddDeckDashboard';
 
 export default function CreateDeck() {
   return (
-    <section className="grid h-screen w-full px-4">
-      <div className="flex flex-col">
-        <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
-          <h1 className="text-xl font-semibold">Add Deck</h1>
-          <Button
-            variant="outline"
-            size="sm"
-            className="ml-auto gap-1.5 text-sm"
-          >
-            <Share className="size-3.5" />
-            Something
-          </Button>
-        </header>
-        <AddDeckDashboard />
-      </div>
+    <section className="h-screen w-full px-4">
+      <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+        <h1 className="text-xl font-semibold">Add Deck</h1>
+      </header>
+
+      <AddDeckDashboard />
     </section>
   );
 }
