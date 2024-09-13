d871abb70e779dd9aab9c4064ce42e33c31729a5
kiel <kielmitchell8@gmail.com>
2024-09-13 09:21:33 +0100

    adds third tab for review

--- components/CardInteractions.tsx
+++ components/CardInteractions.tsx
@@ -3,30 +3,17 @@ import { AddCardDashboard } from './AddCardDashboard';
 import { EditCardDashboard } from './EditCardDashboard';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
-// export const CardInteractionsChildren = ({ Mode, sUser }: { Mode: string, sUser: ISessionUser }) => {
-//   switch (Mode) {
-//     case "Add":
-//       return <AddCardDashboard sessionUser={sUser}/>;
-//     case "Edit":
-//       return <EditCardDashboard sessionUser={sUser}/>;
-//     default:
-//       return <AddCardDashboard sessionUser={sUser}/>;
-//   }
-// };
-
-export const CardInteractions = ({
-  //   Mode,
-  sUser,
-}: {
-  //   Mode: string;
-  sUser: ISessionUser;
-}) => {
+export const CardInteractions = ({ sUser }: { sUser: ISessionUser }) => {
   return (
     <Tabs defaultValue="add" className="w-[40vw]">
-      <TabsList className="grid w-full grid-cols-2">
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger value="review">review</TabsTrigger>
         <TabsTrigger value="add">add</TabsTrigger>
         <TabsTrigger value="edit">edit</TabsTrigger>
       </TabsList>
+      <TabsContent value="review">
+        <h3>review</h3>
+      </TabsContent>
       <TabsContent value="add">
         <AddCardDashboard sessionUser={sUser} />;
       </TabsContent>
