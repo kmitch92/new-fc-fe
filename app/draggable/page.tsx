349b2ac6724fee3231a0875d7917b975f098fc2e
kiel <kielmitchell8@gmail.com>
2024-09-21 18:37:13 +0100

    add resize handles and preliminary width ranges for panels. this needs proper thought though

--- app/draggable/page.tsx
+++ app/draggable/page.tsx
@@ -8,47 +8,64 @@ import { getCardsToReview } from '@/lib/api/handlers';
 
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
+import {
+  DragHandleHorizontalIcon,
+  DragHandleVerticalIcon,
+} from '@radix-ui/react-icons';
+
 export default function Draggable() {
   return (
-    <section className="h-[86vh] w-full">
+    <section className="h-[86vh] w-full px-4">
       <PanelGroup direction="horizontal">
-        <Panel collapsible>
+        {/* {//VERT} */}
+        <Panel maxSize={30} minSize={5}>
           <PanelGroup direction="vertical">
             <Panel className="bg-primary border" minSize={25} maxSize={50}>
               Welcome Card
             </Panel>
-            <PanelResizeHandle />
+            <PanelResizeHandle className="flex flex-row justify-center items-center">
+              <DragHandleHorizontalIcon />
+            </PanelResizeHandle>
             <Panel className="bg-[red] border" minSize={25} maxSize={50}>
               Account Info, credits, notes etc
             </Panel>
-            <PanelResizeHandle />
-            <Panel
-              className="bg-[yellow] border"
-              minSize={25}
-              maxSize={50}
-            ></Panel>
+            <PanelResizeHandle className="flex flex-row justify-center items-center">
+              <DragHandleHorizontalIcon />
+            </PanelResizeHandle>
+            <Panel className="bg-[yellow] border" minSize={25} maxSize={50}>
+              Misc? account settings?
+            </Panel>
           </PanelGroup>
         </Panel>
-        <PanelResizeHandle />
-        <Panel>
+        <PanelResizeHandle className="flex flex-row justify-center items-center">
+          <DragHandleVerticalIcon />
+        </PanelResizeHandle>
+        {/* //VERT */}
+        <Panel maxSize={30} minSize={5}>
           <PanelGroup direction="vertical">
             <Panel className="bg-[magenta] border" maxSize={33} minSize={15}>
               deck management
             </Panel>
-            <PanelResizeHandle />
+            <PanelResizeHandle className="flex flex-row justify-center items-center">
+              <DragHandleHorizontalIcon />
+            </PanelResizeHandle>
             <Panel className="bg-[lightgreen] border" maxSize={85} minSize={67}>
               card browser
             </Panel>
           </PanelGroup>
         </Panel>
-
-        <PanelResizeHandle />
+        <PanelResizeHandle className="flex flex-row justify-center items-center">
+          <DragHandleVerticalIcon />
+        </PanelResizeHandle>
+        {/* //VERT */}
         <Panel>
           <PanelGroup direction="vertical">
             <Panel className="bg-[navy] border" maxSize={33} minSize={15}>
               Metrics
             </Panel>
-            <PanelResizeHandle />
+            <PanelResizeHandle className="flex flex-row justify-center items-center">
+              <DragHandleHorizontalIcon />
+            </PanelResizeHandle>
             <Panel className="bg-[purple] border" maxSize={85} minSize={67}>
               Reviews
             </Panel>
